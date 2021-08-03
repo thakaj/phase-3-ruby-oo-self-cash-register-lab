7b095dde47e27c20ca1e52f59cb0f19ff51b1509
thakaj <taulant.hakaj@scranton.edu>
2021-08-03 15:23:24 -0400

    commit tree 3953784012a1f7806e98e6b0b9e662e17e1c4856

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
-
+class CashRegister
+    attr_accessor :total , :discount , :items, :last_transaction
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+    def add_item(title, price, quantity =1 )
+        self.last_transaction = price * quantity
+        self.total += self.last_transaction
+        quantity.times do 
+            self.items << title
+        end
+    end
+    def apply_discount
+        self.total -= self.discount * self.last_transaction
+    end
+end
